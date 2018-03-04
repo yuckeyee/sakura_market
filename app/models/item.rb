1daa72c18c4e4bbbf9c52e20e0c94e88d514c580
Yuki Kayama <kayama0204@gmail.com>
2018-03-04 15:26:28 +0900

    Make Item model

---
+++ app/models/item.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: items
+#
+#  id          :integer          not null, primary key
+#  name        :string(255)      not null
+#  image       :text(65535)
+#  price       :integer
+#  description :text(65535)
+#  visibility  :boolean          default(FALSE), not null
+#  order       :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Item < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180301222411) do
+ActiveRecord::Schema.define(version: 20180304061538) do
+
+  create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name", null: false
+    t.text "image"
+    t.integer "price"
+    t.text "description"
+    t.boolean "visibility", default: false, null: false
+    t.integer "order"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "email", default: "", null: false
