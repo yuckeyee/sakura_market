3f3897340d1922e68d6afb461c28c222716df1f7
Yuki Kayama <kayama0204@gmail.com>
2018-03-25 15:13:26 +0900

    Add Items controller test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,28 +1,2 @@
 module ApplicationHelper
-  # 3営業日の配列を取得する
-  def get_workdays
-    dates = []
-    n = 3
-    today = nil
-    while true do
-      if today
-        next_day = today.next_day(n)
-        dates << next_day if workday?(next_day)
-      else
-        today = Date.today
-        dates << today if workday?(today)
-      end
-      n += 1
-      break if dates.size == 12
-    end
-    dates
-  end
-
-  def workday?(date)
-    if HolidayJp.holiday?(date) || date.sunday? || date.saturday?
-      false
-    else
-      true
-    end
-  end
 end
