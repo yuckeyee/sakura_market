ec063e600f83fdecf83a2836fc5e1893d98a876d
Yuki Kayama <kayama0204@gmail.com>
2018-03-25 14:00:15 +0900

    Improved flash message UI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
-
+  add_flash_types :success, :info, :warning, :danger
   helper_method :current_cart
 
   rescue_from SecurityError do |_exception|
