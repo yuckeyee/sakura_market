816698b018edea7f3a751c44aa1a79f47254ab35
Yuki Kayama <kayama0204@gmail.com>
2018-04-01 13:22:28 +0900

    Fix users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180324073340) do
+ActiveRecord::Schema.define(version: 20180401041916) do
 
   create_table "cart_items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "quantity", default: 0, null: false
@@ -94,7 +94,7 @@ ActiveRecord::Schema.define(version: 20180324073340) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "role", null: false
+    t.integer "role", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
